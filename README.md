9ffda365f5c541eaaf19c2f8693be68372af930c
Manh Hoang <hamanhhoang1102@gmail.com>
2017-06-03 21:56:23 +0200

    updated project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Spring Boot
+## Spring Boot Reactive
 
 From Local
 
--- src/main/java/com/completablefuture_swagger/config/SwaggerConfig.java
+++ src/main/java/com/completablefuture_swagger/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.mh.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.completablefuture_swagger.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
