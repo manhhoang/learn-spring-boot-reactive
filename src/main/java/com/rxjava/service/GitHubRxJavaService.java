2d9314ebdcdd8e6eb59a7e88e7667028f7c02499
Manh Hoang <hamanhhoang1102@gmail.com>
2017-06-04 20:49:27 +0200

    updated project

--- src/main/java/com/rxjava/service/GitHubRxJavaService.java
+++ src/main/java/com/rxjava/service/GitHubRxJavaService.java
@@ -1,11 +1,15 @@
 package com.rxjava.service;
 
-import com.completablefuture.model.GitHubUser;
+import com.completablefuture.model.GitHubRepo;
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
 import org.springframework.stereotype.Service;
 
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 import java.util.concurrent.Future;
 
 @Service
@@ -13,12 +17,34 @@ public class GitHubRxJavaService {
 
     private final WebTarget target = ClientBuilder.newClient().target("https://api.github.com/");
 
-    public Future<GitHubUser> userAsync(String user) {
+    public void findRepoByUser(String user) {
+        Observable<GitHubRepo> gitHubRepoObservable = Observable.create(emitter -> {
+            try {
+                Future<List<GitHubRepo>> gitHubRepos = reposAsync(user);
+                for (GitHubRepo gitHubRepo : gitHubRepos.get()) {
+                    emitter.onNext(gitHubRepo);
+                }
+                emitter.onComplete();
+            } catch (Exception e) {
+                emitter.onError(e);
+            }
+        });
+
+        Disposable disposable = gitHubRepoObservable.subscribe(gitHubRepo -> System.out.println(gitHubRepo.getName()));
+        disposable.dispose();
+    }
+
+    public Future<List<GitHubRepo>> reposAsync(String user) {
         return target
-                .path("/users/{user}")
+                .path("users/{user}/repos")
                 .resolveTemplate("user", user)
                 .request(MediaType.APPLICATION_JSON_TYPE)
                 .async()
-                .get(GitHubUser.class);
+                .get(new GenericType<List<GitHubRepo>>() { });
+    }
+
+    public static void main(String[] args) {
+        GitHubRxJavaService gitHubRxJavaService = new GitHubRxJavaService();
+        gitHubRxJavaService.findRepoByUser("test");
     }
 }
