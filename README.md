35dfa47765d48fb467fc8af6be69edca59bf7716
Manh Hoang <hamanh.haong@epay.ingenico.com>
2017-06-07 16:53:44 +0200

    added swagger documents

--- README.md
+++ README.md
@@ -13,3 +13,8 @@ docker pull manhhoang/spring-boot-reactive
 docker run -p 8080:8080 -t manhhoang/spring-boot-reactive
 ```
 
+Swagger api documents
+
+http://localhost:8080/v2/api-docs
+
+http://localhost:8080/swagger-ui.html
--- src/main/java/com/completablefuture_swagger/controller/CalculationController.java
+++ src/main/java/com/completablefuture_swagger/controller/CalculationController.java
@@ -2,6 +2,9 @@ package com.completablefuture_swagger.controller;
 
 import com.completablefuture_swagger.model.Task;
 import com.completablefuture_swagger.service.CalculationService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,12 +26,28 @@ public class CalculationController {
         this.calculationService = calculationService;
     }
 
+    @ApiOperation(value = "Create task.", notes = "", response = Task.class, tags = {"Task",})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = Task.class),
+            @ApiResponse(code = 400, message = "Request failed.", response = Task.class),
+            @ApiResponse(code = 401, message = "Authorization has been denied for this request.", response = Task.class),
+            @ApiResponse(code = 422, message = "Model validation failed.", response = Task.class),
+            @ApiResponse(code = 429, message = "Too many requests.", response = Task.class),
+            @ApiResponse(code = 500, message = "Internal server error.", response = Task.class)})
     @RequestMapping(value = "/v1/task", method = RequestMethod.POST, headers = "Accept=application/json")
     @ResponseBody
     public CompletableFuture<Task> createTask(@RequestBody Task task) {
         return this.calculationService.create(task);
     }
 
+    @ApiOperation(value = "Get task.", notes = "", response = Task.class, tags = {"Task",})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = Task.class),
+            @ApiResponse(code = 400, message = "Request failed.", response = Task.class),
+            @ApiResponse(code = 401, message = "Authorization has been denied for this request.", response = Task.class),
+            @ApiResponse(code = 422, message = "Model validation failed.", response = Task.class),
+            @ApiResponse(code = 429, message = "Too many requests.", response = Task.class),
+            @ApiResponse(code = 500, message = "Internal server error.", response = Task.class)})
     @RequestMapping(value = "/v1/task/{taskId}", method = RequestMethod.GET, headers = "Accept=application/json")
     @ResponseBody
     public CompletableFuture<Task> getTask(@PathVariable("taskId") String taskId) {
