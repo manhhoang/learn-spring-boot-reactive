0fc3ed169b224c9a71f085c18103edfe3aa19d82
Manh Hoang <hamanh.haong@epay.ingenico.com>
2017-07-07 16:16:22 +0200

    updated

--- src/main/java/com/spring_standalone/config/AppConfig.java
+++ src/main/java/com/spring_standalone/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.spring_standalone.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 
 @ComponentScan("com.spring_standalone")
 @EnableAutoConfiguration
+@EnableCaching
 @Configuration
 public class AppConfig {
 
--- src/main/java/com/spring_standalone/service/TheMovieDBService.java
+++ src/main/java/com/spring_standalone/service/TheMovieDBService.java
@@ -1,12 +1,12 @@
 package com.spring_standalone.service;
 
-import com.reactive.service.CompletableFutureService;
 import com.spring_standalone.exception.AppException;
 import com.spring_standalone.model.Movie;
 import com.spring_standalone.model.TheMovieDB;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,11 +18,12 @@ public class TheMovieDBService {
     private static final String THE_MOVIE_DB_URL =
             "https://api.themoviedb.org/3/search/movie?api_key=ea7ba32805fa642f35979db217a46d59&query={movieName}";
 
-    private static final Logger logger = LoggerFactory.getLogger(CompletableFutureService.class);
+    private static final Logger logger = LoggerFactory.getLogger(TheMovieDBService.class);
 
     @Autowired
     private RestTemplate restTemplate;
 
+    @Cacheable("movies")
     public CompletableFuture<TheMovieDB> search(String movieName) {
         return CompletableFuture.supplyAsync(() -> restTemplate.getForEntity(THE_MOVIE_DB_URL, TheMovieDB.class, movieName))
                 .thenApply(theMovieDB -> {
@@ -36,8 +37,8 @@ public class TheMovieDBService {
                     }
                     return retTheMovieDB;
                 }).exceptionally(e -> {
-                    logger.error("Failed to get movie");
-                    throw new AppException("100", "Failed to get movie");
+                    logger.error("Failed to get movie from api.themoviedb.org");
+                    throw new AppException("100", "Failed to get movie from api.themoviedb.org");
                 });
     }
 }
